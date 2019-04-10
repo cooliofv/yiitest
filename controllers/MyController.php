e50d7fd2a553b9ee750b6b28140d491844893c80
cooliofv <cooliofv@gmail.com>
2019-04-10 18:08:45 +0300

    first

---
+++ controllers/MyController.php
@@ -0,0 +1,22 @@
+<?php
+
+
+namespace app\controllers;
+
+
+class MyController extends AppController
+{
+    public function actionIndex($id = null)
+    {
+        $hi = 'Hello world!';
+        $names = [
+            'Ivanov',
+            'Prtrov',
+            'Sidorov'
+        ];
+
+        $this->debug($names);
+
+        return $this->render('index',compact('hi', 'names','id'));
+    }
+}
---
+++ views/my/index.php
@@ -0,0 +1,9 @@
+<h1><?php echo $hi ?> INDEX!</h1>
+
+<?php foreach ($names as $name) {
+    echo $name.'<br>';
+}
+
+echo $id;
+
+?>
